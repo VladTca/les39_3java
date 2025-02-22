a9de2897cf704245489a17c1a4a09c497ce15237
VladTca <votsats@gmail.com>
2025-02-22 05:57:03 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = "Jack,John,Nick,John";
+        uniqStr(string);
+
+    }
+
+    private static void uniqStr(String string) {
+//        String[] words = string.split(",");
+        Set<String> setStr=new LinkedHashSet(Arrays.asList(string.split(",")));
+//        for (int i = 0; i < words.length; i++) {
+//            setStr.add(words[i]);
+//        }
+//        StringBuilder sb = new StringBuilder();
+//        for (Object o : setStr) {
+//            sb.append(o);
+//            sb.append(",");
+//        }
+        //System.out.println(sb);
+        System.out.println(String.join(",", setStr));
+    }
+}
